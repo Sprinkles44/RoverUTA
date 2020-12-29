33af0c37b62922f0423373707f7fb4d600238c47
Oscar Alvarez <mathoscaralvarez@gmail.com>
2020-12-28 19:10:47 -0600

    v1.0 PC client working video stream initial upload

--- Server/pc_client.py
+++ Server/pc_client.py
@@ -1,9 +1,12 @@
+# host_ip = '192.168.1.108' # IP of Rover using WiFi
+
+
 import socket, cv2, pickle, struct
 import numpy as np
 
 ## Create Socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_ip = '192.168.1.137' # IP of Rover
+host_ip = '192.168.1.108' # IP of Rover
 port = 9999
 client_socket.connect((host_ip, port)) # tuple
 data = b""
@@ -13,7 +16,7 @@ payload_size = struct.calcsize("Q")
 def get_frame():
   global client_socket, host_ip, port, data, payload_size
   while len(data) < payload_size:
-    packet = client_socket.recv(4*1024) # 4kB
+    packet = client_socket.recv(4*1024) # 4 kB
     if not packet: break
     data += packet
 
@@ -53,25 +56,27 @@ frame = get_frame()
 color_frame = frame
 
 while True:
-  frame = get_frame()
+  try:
+    frame = get_frame()
+    # Finding dimensions for Window Size
+    f_height = frame.shape[0]
+    f_width = frame.shape[1]  
 
-  # Finding dimensions for Window Size
-  f_height = frame.shape[0]
-  f_width = frame.shape[1]  
-  
-  cv2.namedWindow('Recieved', cv2.WINDOW_NORMAL)
-  cv2.imshow('Received', frame)
-  
-  ## Get color
-  cv2.setMouseCallback('Received', get_color_func)
+    cv2.namedWindow('Recieved', cv2.WINDOW_NORMAL)
+    cv2.imshow('Received', frame)
 
-  if clicked:
-    low_color = np.array([b-color_thresh, g-color_thresh, r-color_thresh])
-    high_color = np.array([b+color_thresh, g+color_thresh, r+color_thresh])
-    color_mask = cv2.inRange(frame, low_color, high_color)
-    # Show frame(s)
-    color_frame = cv2.bitwise_and(frame, frame, mask=color_mask)
-    cv2.imshow('Red Mask', color_frame)
+    ## Get color
+    cv2.setMouseCallback('Received', get_color_func)
+
+    if clicked:
+      low_color = np.array([b-color_thresh, g-color_thresh, r-color_thresh])
+      high_color = np.array([b+color_thresh, g+color_thresh, r+color_thresh])
+      color_mask = cv2.inRange(frame, low_color, high_color)
+      # Show frame(s)
+      color_frame = cv2.bitwise_and(frame, frame, mask=color_mask)
+      cv2.imshow('Red Mask', color_frame)
+  except:
+    pass
 
   key = cv2.waitKey(1) & 0xFF
   if key == ord('q'):
@@ -79,3 +84,4 @@ while True:
 
 cv2.destroyAllWindows()
 client_socket.close()
+close()
