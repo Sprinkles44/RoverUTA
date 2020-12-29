cacac1b7a1d8498bfa59f127581bf23eba781377
Oscar Alvarez <mathoscaralvarez@gmail.com>
2020-12-28 19:09:07 -0600

    v1.0 working jetson video server initial upload

--- Server/rover_server.py
+++ Server/rover_server.py
@@ -4,8 +4,9 @@ import socket, cv2, pickle, struct
 
 # Socket create
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_name = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
+#host_name = socket.getfqdn()
+#host_ip = socket.gethostbyname(host_name)
+host_ip = '192.168.1.108'
 print('HOST IP: ', host_ip)
 port = 9999
 socket_address = (host_ip,port)
@@ -22,17 +23,20 @@ while True:
   client_socket, addr = server_socket.accept()
   print("GOT CONNECTION FROM: ", addr)
   if client_socket:
-    vid = cv2.VideoCapture(1)
+    vid = cv2.VideoCapture(0)
     while(vid.isOpened()):
       ret, frame = vid.read()
-      a = pickle.dumps(frame)
-      message = struct.pack("Q", len(a)) + a
-      client_socket.sendall(message)
+      if(ret):
+        a = pickle.dumps(frame)
+        message = struct.pack("Q", len(a)) + a
+        client_socket.sendall(message)
 
-      ## Play video on raspi, enable for debugging
-      ## (will use processing power, disable when implimenting on rover)
-      # cv2.imshow('TRANSMITTING VIDEO', frame) 
+      ## vvv Play video, enable for debugging vvv
+      ## vvv (will use processing power, disable when implimenting on rover) vvv
+      cv2.imshow('TRANSMITTING VIDEO', frame) 
       
       key = cv2.waitKey(1) & 0xFF
       if key == ord('q'):
+        vid.release()
         client_socket.close()
+        exit()
